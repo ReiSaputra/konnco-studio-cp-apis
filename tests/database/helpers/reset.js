842f6bf7be88d8a591cd50a37652b06ed83e11c6
Muhammad Fathurraihan Saputra <121397266+ReiSaputra@users.noreply.github.com>
2025-07-22 10:07:15 +0700

    [ON-PROGRESS] Schema Prisma - Changing data type 'lenghtOfService' from Int? to String? on model Application

---
+++ tests/database/helpers/reset.js
@@ -0,0 +1,13 @@
+import { prisma } from "../../../src/database.js";
+
+const resetAllData = async () => {
+  await prisma.application.deleteMany();
+  await prisma.career.deleteMany();
+  await prisma.blog.deleteMany();
+  await prisma.inquiry.deleteMany();
+  await prisma.admin.deleteMany();
+
+  console.info("All data has been reset");
+};
+
+resetAllData();
