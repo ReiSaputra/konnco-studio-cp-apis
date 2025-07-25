5660892be761b918dbed70b4c8216e5883c51027
Muhammad Fathurraihan Saputra <121397266+ReiSaputra@users.noreply.github.com>
2025-07-25 10:38:54 +0700

    [ON-PROGRESS] Admin features - Hot Fix Admin Route

--- src/routes/admin-route.js
+++ src/routes/admin-route.js
@@ -1,18 +1,17 @@
 import express from "express";
-import { loginAdminController, dashboardAdminController, getAdminBlogsController, getAdminBlogDetailController } from "../controllers/admin-controller.js";
+import { loginAdminController, dashboardAdminController, getAdminBlogController, getAdminBlogDetailController } from "../controllers/admin-controller.js";
 import { authMiddleware } from "../middlewares/auth-middleware.js";
 
 const adminRoute = express.Router();
 
 // Auth
-// adminRoute.post("/admins/auth/login", loginAdminController);
-adminRoute.post("/auth/admins/login", loginAdminController);
+adminRoute.post("/admins/auth/login", loginAdminController);
 
 // Dashboard
 adminRoute.get("/admins/dashboard/overview", authMiddleware, dashboardAdminController);
 
 // Blogs
-adminRoute.get("/admins/blogs", authMiddleware, getAdminBlogsController);
+adminRoute.get("/admins/blogs", authMiddleware, getAdminBlogController);
 adminRoute.get("/admins/blogs/:blogSlug", authMiddleware, getAdminBlogDetailController);
 
 // Careers
