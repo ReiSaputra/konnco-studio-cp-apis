a198ec04f3526aa6953b505f187e631af11346a2
Muhammad Fathurraihan Saputra <121397266+ReiSaputra@users.noreply.github.com>
2025-07-29 15:16:31 +0700

    [HOTFIX] Admin Features - POST /api/v1/admins/blogs changing response error 'already is exists'

--- src/services/admin-service.js
+++ src/services/admin-service.js
@@ -256,7 +256,7 @@ const createAdminBlogService = async (role, permissions, title, content, photoNa
         where: { slug: slug },
       });
 
-      if (findData) throw new Error("Blog already exists");
+      if (findData) throw new Error("Blog Slug already exists");
 
       createData = await prisma.blog.create({
         data: {
